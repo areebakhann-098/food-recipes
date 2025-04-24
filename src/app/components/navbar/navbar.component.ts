d566512a968c932b1466d56c0e64b8bedafa69fe
areeba-wq <areebakhalid9854@gmail.com>
2025-04-24 11:12:07 +0500

    update

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,15 +3,23 @@ import { TranslationService } from '../../translation.service';
 
 import { TranslateModule } from '@ngx-translate/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-navbar',
-  imports: [TranslateModule, RouterModule],
+  imports: [TranslateModule, RouterModule, ButtonModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  menuOpen = false;
+  
+
   constructor(private translationService: TranslationService) {}
   changeLanguage(lang: string) {
     this.translationService.translateText(lang);
   }
+  
 }
+
+
